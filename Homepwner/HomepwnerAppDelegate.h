2d5437bbdb8647b9b7fab77574166d5479d80527
Dave Wilde <david.wilde@racksapce.com>
2011-06-11 06:59:44 -0500

    Initial Commit

---
+++ Homepwner/HomepwnerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  HomepwnerAppDelegate.h
+//  Homepwner
+//
+//  Created by Dave Wilde on 6/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomepwnerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
